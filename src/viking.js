6be0339dcf821d8ae2818db0cde028af6f2f0a94
Stephane Ds <92688358+Naneeee@users.noreply.github.com>
2022-05-16 20:58:21 +0200

    solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,74 @@
 // Soldier
-class Soldier {}
+class Soldier {
+constructor(health,strength){
+  this.health=health
+  this.strength=strength
+}
+attack(){
+  return this.strength
+}
+receiveDamage(damage){
+  this.health=this.health-damage
+}
+}
+class Viking extends Soldier{
+  constructor(name,health,strength){
+    super(health,strength)
+    this.name=name
+  }
 
-// Viking
-class Viking {}
+receiveDamage(damage){
+this.health=this.health-damage
+  if(this.health > 0 ){
+    return `${this.name} has received ${damage} points of damage`
+  }
+  else{
+    return `${this.name} has died in act of combat`
+    
+  }
+
+}
+battleCry(){
+  return "Odin Owns You All!"
+}
+}
+
+
+
 
 // Saxon
-class Saxon {}
+class Saxon  extends Soldier{
+  receiveDamage(damage){
+    this.health=this.health-damage
+    if(this.health > 0){
+      return `A Saxon has received ${damage} points of damage`;
+    }
+    else{
+       return `A Saxon has died in combat`;
+    }
+  }
+}
 
 // War
-class War {}
+class War {
+  vikingArmy = []
+  saxonArmy = []
+  
+
+  addViking()
+  }
+
+addSaxon()
 
 
+vikingAttack()
+saxonAttack()
+showStatus()
+}
+
+let a = new Soldier(100, 24)
+let b = new Soldier(100, 50)
+console.log(b.attack())
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
